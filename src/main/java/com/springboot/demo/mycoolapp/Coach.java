2337801eb3b54c55bafef03ce46aacf3e599dbca
Menna Emam <menna20101@gmail.com>
2024-10-21 22:00:07 +0300

    07-constructor-injection

---
+++ src/main/java/com/springboot/demo/mycoolapp/Coach.java
@@ -0,0 +1,5 @@
+package com.springboot.demo.mycoolapp;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/springboot/demo/mycoolapp/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.springboot.demo.mycoolapp.rest;
+
+import com.springboot.demo.mycoolapp.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach coach){
+        myCoach = coach;
+    }
+
+    @GetMapping("/daily-workout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
