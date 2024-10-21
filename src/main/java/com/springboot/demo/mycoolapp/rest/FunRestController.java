cf51f90d662ae281c1c5d9296ee101eca6a5bc3c
Menna Emam <menna20101@gmail.com>
2024-10-21 21:40:13 +0300

    06-properties-demo

--- src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,11 +1,20 @@
 package com.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-    // expose "/" that return "Hello World"
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeaInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
@@ -13,7 +22,7 @@ public class FunRestController {
     }
 
     @GetMapping("/workout")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return "Run a hard 5K!";
     }
 }
