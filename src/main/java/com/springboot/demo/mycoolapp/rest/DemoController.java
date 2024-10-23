1ad77c76899241ef586e4f0e97b41e31c2e25ffa
Menna Emam <menna20101@gmail.com>
2024-10-23 21:48:26 +0300

    13-bean-scopes

--- src/main/java/com/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/springboot/demo/mycoolapp/rest/DemoController.java
@@ -10,16 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
+    private Coach anotherCoach;
 
     @Autowired
-    public DemoController(@Qualifier("baseballCoach") Coach coach){
+    public DemoController(
+            @Qualifier("cricketCoach") Coach coach,
+            @Qualifier("cricketCoach") Coach coach2){
         System.out.println("In constructor: " + getClass().getSimpleName());
 
         myCoach = coach;
+        anotherCoach = coach2;
     }
 
     @GetMapping("/daily-workout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
