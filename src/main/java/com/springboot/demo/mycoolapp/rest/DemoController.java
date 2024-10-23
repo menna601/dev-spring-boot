d1d3de42405aefd56035160aef83d2466dabbfdd
Menna Emam <menna20101@gmail.com>
2024-10-23 21:32:51 +0300

    12-lazy-initialization

--- src/main/java/com/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/springboot/demo/mycoolapp/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.springboot.demo.mycoolapp.rest;
 
 import com.springboot.demo.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,9 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach coach){
+    public DemoController(@Qualifier("baseballCoach") Coach coach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+
         myCoach = coach;
     }
 
